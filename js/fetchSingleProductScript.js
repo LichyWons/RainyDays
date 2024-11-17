cca42afe2fb0cab75dd10acd8da3499ee6e70efc
LichyWons <124511088+LichyWons@users.noreply.github.com>
2024-11-17 21:20:12 +0100

    fixed js

---
+++ js/fetchSingleProductScript.js
@@ -0,0 +1,95 @@
+const spinner = document.getElementById('spinner');
+spinner.style.display = 'block';
+
+const productContainer = document.getElementById('productCard');
+const URL = "https://cors.noroff.dev/http://krzysztofbytniewski.com/wp-json/wc/store/products/";
+
+async function fetchAndDisplaySingleProduct() {
+    const productId = new URLSearchParams (window.location.search).get("id");
+    const response = await fetch(URL + productId);
+    const product = await response.json();
+    spinner.style.display =`none`;
+    displaySingleProduct(product);
+    console.log(product, productId)
+    
+}
+
+
+function displaySingleProduct(product) {
+    productContainer.innerHTML = `
+   
+    <img src = "${product.images[0].src}"/>
+    <h2>${product.name}</h2>
+    <h3>NOK ${product.prices.price}</h3>
+    <a>${product.description}</a>
+    <a href="list.html" class="button">Add to cart</a>`
+ 
+    }
+
+    fetchAndDisplaySingleProduct();
+    
+console.log(product.images)
+
+
+
+
+
+
+
+
+/*const url =
+ ("https://cors.noroff.dev/http://krzysztofbytniewski.com/wp-json/wc/store/products/" + id);
+
+const productCard = document.querySelector("#productCard");
+
+async function getProducts() {
+ try {
+   const response = await fetch(url);
+   if (!response.ok) {
+     throw new Error("Network response was not ok");
+   }
+   const products = await response.json();
+   displayProducts(products);
+ } catch (error) {
+   console.log(error);
+   productCard.innerHTML = error;
+ }
+}
+
+getProducts();
+
+function displayProducts(products) {
+ products.forEach((product) => {
+   let divProduct = document.createElement("div");
+   divProduct.className = "productCard";
+   let link1 = document.createElement("a");
+   link1.href = "/productpage.html?id=" + product.id;
+   let image = document.createElement("img");
+   image.src = product.images[0].thumbnail;
+   let p1 = document.createElement("p");
+   p1.className = "productMake";
+   p1.innerText = product.gender;
+   let p2 = document.createElement("p");
+   p2.className = "productModel";
+   p2.innerText = product.name;
+   let p3 = document.createElement("p");
+   p3.className = "productPrice";
+   p3.innerHTML = "kr " + product.prices.price;
+   let link2 = document.createElement("a");
+   link2.className = "moreButton";
+   link2.href = "/productpage.html?id=" + product.id;
+   link2.innerHTML = "More  &gt;";
+   link1.appendChild(image);
+   //link1.appendChild(p1);
+   link1.appendChild(p2);
+   link1.appendChild(p3);
+   divProduct.appendChild(link1);
+   divProduct.appendChild(link2);
+   productContainer.appendChild(divProduct);
+ });
+}
+ */
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,17 +1,75 @@
 
+ const spinner = 
+document.getElementById('spinner');
+ spinner.style.display = 'block';
+
+
+const url =
+  "https://cors.noroff.dev/http://krzysztofbytniewski.com/wp-json/wc/store/products";
+
+const productContainer = document.querySelector("#productContainer");
+
+async function getProducts() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    console.log(error);
+    productContainer.innerHTML = error;
+  }
+}
+
+getProducts();
+
+function displayProducts(products) {
+  products.forEach((product) => {
+    let divProduct = document.createElement("div");
+    divProduct.className = "productCard";
+    let link1 = document.createElement("a");
+    link1.href = "/productpage.html?id=" + product.id;
+    let image = document.createElement("img");
+    image.src = product.images[0].thumbnail;
+    let p1 = document.createElement("p");
+    p1.className = "productMake";
+    p1.innerText = product.gender;
+    let p2 = document.createElement("p");
+    p2.className = "productModel";
+    p2.innerText = product.name;
+    let p3 = document.createElement("p");
+    p3.className = "productPrice";
+    p3.innerHTML = "kr " + product.prices.price;
+    let link2 = document.createElement("a");
+    link2.className = "moreButton";
+    link2.href = "/productpage.html?id=" + product.id;
+    link2.innerHTML = "More  &gt;";
+    link1.appendChild(image);
+    //link1.appendChild(p1);
+    link1.appendChild(p2);
+    link1.appendChild(p3);
+    divProduct.appendChild(link1);
+    divProduct.appendChild(link2);
+    productContainer.appendChild(divProduct);
+  });
+  spinner.style.display = 'none';
+}
+ 
+
+
+
+
+
 /*document.addEventListener ("DOMContentLoaded", function() )
-  // Select the spinner element
-  const spinner = document.getElementById('spinner');
-
-  // Show the spinner before fetching data
-  spinner.style.display = 'block';
-
+ 
   fetch('https://krzysztofbytniewski.com/wp-json/wc/store/products')
   .then(response => response.json())
   .then(data => displayProducts(data))
   .catch(error => console.error('Error fetching the products:', error));
   
-*/
+
  
   
 
@@ -68,35 +126,11 @@ fetch('https://cors.noroff.dev/http://krzysztofbytniewski.com/wp-json/wc/store/p
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 .catch(error => {
   console.error('There was a problem with the fetch operation:', error);
   document.querySelector("#productContainer").innerHTML = error.message;
 }) 
-.finally(() => {
-  spinner.style.display = 'none';
-});
+
 ;
 
 
@@ -153,3 +187,6 @@ fetch('https://cors.noroff.dev/http://krzysztofbytniewski.com/wp-json/wc/store/p
 .catch(error => {
   console.error('There was a problem with the fetch operation:', error);
 });
+*/
+
+
